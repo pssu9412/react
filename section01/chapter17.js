1937aac152a79c9e35163b3d5c54f7e16f7d193b
pssu9412 <153481832+pssu9412@users.noreply.github.com>
2024-10-23 07:32:29 +0900

    배열
    생성 및 요소 접근

---
+++ section01/chapter17.js
@@ -0,0 +1,16 @@
+// 배열 생성
+
+let arrA = new Array();
+let arrB =[];
+
+let arrC = [1,2,3,true, "hello",null,undefined];
+
+// 배열 요소 접근
+
+let item = arrC[0];
+let item2 = arrC[2];
+
+console.log(item,item2);
+
+arrC[0] = "bye";
+console.log(arrC);
