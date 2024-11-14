fc3f1b3cdd18934d6cd745447ebd159300102f41
pssu9412 <153481832+pssu9412@users.noreply.github.com>
2024-11-14 13:43:41 +0900

    투두리스트 기능구현
    투두 추가
    투두 렌더링(검색)
    체크박스 기능
    투두 삭제

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Editor from './components/Editor'
@@ -27,6 +27,41 @@ const mockData =[
 
 function App() {
   const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date : new Date().getTime()
+    }
+
+    setTodos([newTodo, ...todos]);
+  };
+
+  // 체크박스 관련 함수
+  const onUpdate = (targetId) => {
+    // todos State 값들 중에 targetId와 일치하는 id를 갖는 투두아이템 isDone 값 변경
+
+    /*   삼항연산자 사용 
+    setTodos(todos.map((todo)=> 
+      todo.id === targetId ? {...todo, isDone: !todo.isDone} : todo ))
+    */
+    setTodos(todos.map((todo)=>{
+      if(todo.id === targetId){
+        return {
+          ...todo,
+          isDone: !todo.isDone
+        }
+      }
+      return todo;
+    }))
+  }
+
+  const onDelete= (targetId) =>{
+    setTodos(todos.filter((todo)=> todo.id !== targetId));
+  }
 
   return (
     <div className="App">
@@ -34,10 +69,10 @@ function App() {
         <Header />
       </section>
       <section>
-        <Editor />
+        <Editor onCreate={onCreate}/>
       </section>
       <section>
-        <List />
+        <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
       </section>
     </div>
   )
--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,9 +1,38 @@
 import './Editor.css'
-const Editor = () => {
+import {useState, useRef } from "react";
+
+const Editor = ({onCreate}) => {
+    const [content, setContent]= useState("");
+    const contentRef= useRef();
+
+    const onChangeContent =(e)=>{
+        setContent(e.target.value);
+    }
+
+    const onKeydown =(e)=>{
+        if(e.keyCode === 13){
+            onSubmit;
+        }
+    }
+
+    const onSubmit = ()=>{
+        if(content ===""){
+            contentRef.current.focus(); // 입력칸 포커스
+            return;
+        }
+        onCreate(content);
+        setContent(""); // 입력칸 비워주기
+    }
+    
     return (
         <div className="Editor">
-            <input placeholder="새로운 Todo..." />
-            <button>추가</button>
+            <input 
+                ref={contentRef}
+                value={content}
+                onKeyDown={onKeydown}
+                onChange={onChangeContent}
+                placeholder="새로운 Todo..." />
+            <button onClick={onSubmit}>추가</button>
         </div>
     )
 }
