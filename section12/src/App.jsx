867464bc129c1693e2c833fcff3c487c90caffed
pssu9412 <153481832+pssu9412@users.noreply.github.com>
2024-12-03 11:16:46 +0900

    페이지 및 기능 구현
    일기 관리 기능 구현
    Home페이지 ui, 기능 구현

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
+import { useReducer, useRef, createContext } from 'react';
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
+import Edit from "./pages/Edit";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
 // import emotion1 from './assets/emotion1.png';
@@ -11,17 +13,89 @@ import { Routes, Route, Link, useNavigate } from "react-router-dom";
 // import emotion4 from './assets/emotion4.png';
 // import emotion5 from './assets/emotion5.png';
 
-import { getEmotionImage } from './util/get-emotion-image';
+// import { getEmotionImage } from './util/get-emotion-image';
+// import Button from "./components/Button";
+// import Header from "./components/Header"
+
 
 // 1. "/" : 모든 일기를 조회하는 home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 new 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 diary 페이지
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date("2024-11-27").getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date("2024-11-26").getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date("2024-10-22").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
+  }
+]
+
+function reducer(state,action){
+  switch(action.type){
+    case "CREATE" : return [action.data, ...state];
+    case "UPDATE" : 
+      return state.map(
+        (item)=>String(item.id) === String(action.data.id) ? action.data : item);
+    case "DELETE" : return state.filter((item)=> String(item.id) !== String(action.id));
+    default: return state;
+  }
+
+}
+
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
+
 function App() {
   const nav = useNavigate();
   // 페이지 이동할 때 Link 대신 쓸 수 있는 Hook
   const onClickButton= ()=>{
     nav('/new');
   }
+
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(4);
+
+  // 새로운 일기 추가
+  const onCreate = (createdDate, emotionId, content)=>{
+    dispatch({
+      type:"CREATE",
+      data:{
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    })
+  }
+  // 기존 일기 수정
+  const onUpdate= (id, createdDate, emotionId, content)=>{
+    dispatch({
+      type: "UPDATE",
+      data:{
+        id, createdDate, emotionId, content
+      }
+    })
+  }
+  // 기존 일기 삭제
+  const onDelete =(id) =>{
+    dispatch({
+      type: "DELETE",
+      id
+    })
+  }
+
   return (
     <>
       {/* 
@@ -31,28 +105,62 @@ function App() {
       <div>
         <img src={'/emotion.png'} />
       </div> */}
-      <div>
+      {/* <div>
         <img src={getEmotionImage(1)} />
         <img src={getEmotionImage(2)} />
         <img src={getEmotionImage(3)} />
         <img src={getEmotionImage(4)} />
         <img src={getEmotionImage(5)} />
-      </div>
+      </div> */}
+{/* 
+      <Button 
+        text={123} 
+        onClick={()=>{console.log('123버튼클릭')}}
+        />
+      <Button 
+        text={123} 
+        type={"POSITIVE"}
+        onClick={()=>{console.log('123버튼클릭')}}
+      />
+      <Button 
+        text={123} 
+        type={"NEGATIVE"}
+        onClick={()=>{console.log('123버튼클릭')}}
+      /> */}
+{/* 
+      <button onClick={()=>{
+        onCreate(new Date().getTime(), 1, "Hello");
+      }}>일기 추가 </button>
+
+      <button onClick={()=>{
+        onUpdate(1, new Date().getTime(), 3, "수정된 일기입니다.");
+      }}>일기 수정</button>
+
+      <button onClick={()=>{
+        onDelete(1);
+      }}>일기 삭제</button> */}
       {/* Routes 컴퍼넌트 밖 요소는 모든 페이지에서 렌더링 됨 */}
-      <div>
-        {/* Link : html에서 사용하는 <a> 태그와 같음 */}
+{/* <div>
+        Link : html에서 사용하는 <a> 태그와 같음
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
         <Link to={"/diary"}>Diary</Link>
       </div>
-      <button onClick={onClickButton}> New 페이지로 이동 </button>
+      <button onClick={onClickButton}> New 페이지로 이동 </button> */}
+
       {/* Routes 컴퍼넌트 안에는 Route 컴퍼넌트만 들어갈 수 있음 */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider 
+          value={(onCreate, onUpdate, onDelete)}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   )
 }
