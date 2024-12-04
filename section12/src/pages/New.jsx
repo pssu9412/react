d978c905b810ba5ef86160ce33047f65ebe5bade
pssu9412 <153481832+pssu9412@users.noreply.github.com>
2024-12-04 13:46:20 +0900

    new페이지 ui, 기능 구현
    작성 및 취소 구현
    날짜, 감정 선택 기능 구현

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,9 +1,26 @@
+import Header from "../components/Header"
+import Button from "../components/Button"
+import Editor from "../components/Editor"
+import { useNavigate } from "react-router-dom"
+import { useContext } from "react"
+import { DiaryDispatchContext } from "../App"
 
 
 const New = ()=>{
+    const {onCreate} = useContext(DiaryDispatchContext);
+    const nav = useNavigate();
+
+    const onSubmit =(input) =>{
+        console.log(onCreate);
+        onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+        nav('/', { replace:true });
+    }
+
     return (
         <div>
-            n
+            <Header title={"새일기 쓰기"}
+                leftChild={<Button onClick={()=>nav(-1)} text={"< 돌아가기"}/>} />
+            <Editor onSubmit={onSubmit} />
         </div>
     )
 }
